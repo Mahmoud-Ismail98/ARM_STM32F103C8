7adf24f44daebaafc15d320fb5fb8cac4c71c133
Mahmoud-Koraim <m.koraim77@gmail.com>
2020-09-27 18:50:59 +0200

    Serial Periphles Interface (SPI) Driver

--- 01-LIB/01-STD_TYPES/STD_TYPES.h
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -1,8 +1,10 @@
 /*********************************************************/
 /********** Author 		: Mahmoud Korayem ****************/
-/********** Date        : 14 AUG 2020     ****************/
-/********** version     : V 01			  ****************/
-/********** Description : STD_TYPES.h      ****************/
+/********** Date        : 26 SEP 2020     ****************/
+/********** version     : V 02			  ****************/
+/********** LOG			:v 01 -> Intial Creation *********/
+/********** 			:v 01 -> Add to Signed Data*******/
+/********** Description : STD_TYPES.h     ****************/
 /*********************************************************/
 
 #ifndef STD_TYPES_H
@@ -12,7 +14,9 @@ typedef  unsigned char        u8 ;
 typedef  unsigned short int   u16;
 typedef  unsigned long  int   u32;
 
-
+typedef  signed char          s8 ;
+typedef  signed short int     s16;
+typedef  signed long  int     s32;
 
 
 #endif
