7e374892252b112752b402b6e973be43ac6dcae9
Mahmoud-Koraim <m.koraim77@gmail.com>
2022-03-21 13:25:41 +0200

    Alternate function I/O and debug configuration (AFIO) driver

---
+++ 02-MCAL/09-AFIO/AFIO_interface.h
@@ -0,0 +1,12 @@
+/*********************************************************/
+/********** Author 		: Mahmoud Korayem ****************/
+/********** Date        : 28 FEB 2022     ****************/
+/********** version     : V 01			  ****************/
+/*********************************************************/
+
+#ifndef AFIO_INTERFACE_H
+#define AFIO_INTERFACE_H
+
+void MAFIO_voidSetEXTILineNum(u8 Copy_u8ExtiLine , u8 Copy_u8ExtiPinNum);
+
+#endif
