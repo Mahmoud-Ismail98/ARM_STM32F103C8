f2dd1e4831ee96fcffff4847909303ec5e8c7679
Mahmoud-Koraim <m.koraim77@gmail.com>
2022-03-28 03:01:33 +0200

    updated driver of LED-MATRIX

--- 03-HAL/01-LED-MATRIX/LEDMAT_interface.h
+++ 03-HAL/01-LED-MATRIX/LEDMAT_interface.h
@@ -1,13 +1,13 @@
 /*********************************************************/
 /********** Author 		: Mahmoud Korayem ****************/
-/********** Date        : 15 SEP 2020     ****************/
-/********** version     : V 01			  ****************/
+/********** Date        : 18 mar 2022     ****************/
+/********** version     : V 02			  ****************/
 /********** Description : LEDMAT_INTERFACE_H**************/
 
 #ifndef  LEDMAT_INTERFACE_H
 #define  LEDMAT_INTERFACE_H
 
 void HLEDMAT_voidInit(void);
-void HLEDMAT_voidDisplay(u8 *Copy_u8Date);
+void HLEDMAT_voidDisplay(u8 *Copy_u8Date,u8 Copy_u8SizeArr);
 
 #endif
